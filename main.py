41c9a8a60984dede785b0435e12e567f984a9d7c
Fabaian1 <130661100+Fabaian1@users.noreply.github.com>
2023-04-23 14:54:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import random
+
+while True:
+  print(random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),":",random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9),random.randint(0, 9))
